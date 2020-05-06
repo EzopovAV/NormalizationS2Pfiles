26bf499c30949cccde2b9d35c94a4b1cbd22717d
EzopovAV <EzopovAV@mail.ru>
2020-05-06 21:54:02 +0400

    Add class NormalizerS2Pfiles

---
+++ normalizerS2Pfiles/normalizerS2Pfiles/NormalizerS2Pfiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace normalizerS2Pfiles
+{
+	class NormalizerS2Pfiles
+	{
+		public string[] Normalize(string[] s)
+		{
+			return s;
+		}
+	}
+}
--- normalizerS2Pfiles/normalizerS2Pfiles/Program.cs
+++ normalizerS2Pfiles/normalizerS2Pfiles/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace normalizerS2Pfiles
 	{
 		static void Main(string[] args)
 		{
+			string pathSource = @"C:\petProjects\s2p files\E5071B GPPM-_Chanel-1_Rx_ATTen-0_PHase-2.s2p";
+			string pathResult = @"C:\petProjects\s2p files\E5071B GPPM-_Chanel-1_Rx_ATTen-0_PHase-2_nomalized.s2p";
+
+			string[] Source = File.ReadAllLines(pathSource);
+
+			NormalizerS2Pfiles x = new NormalizerS2Pfiles();
+
+			string[] Result = x.Normalize(Source);
+
+			File.WriteAllLines(pathResult, Result);
 		}
 	}
 }
