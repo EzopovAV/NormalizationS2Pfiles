a2a95fbb1f00744492415d0d5dd54b42356d4595
EzopovAV <EzopovAV@mail.ru>
2020-07-17 14:40:13 +0400

    Move Sample to separate class

--- normalizerS2Pfiles/normalizerS2Pfiles/NormalizerS2P.cs
+++ normalizerS2Pfiles/normalizerS2Pfiles/NormalizerS2P.cs
@@ -254,17 +254,4 @@ namespace normalizerS2Pfiles
 			return samples;
 		}
 	}
-
-	struct Sample
-	{
-		public double Freq;
-		public double S11MagOrRe;
-		public double S11AngOrIm;
-		public double S12MagOrRe;
-		public double S12AngOrIm;
-		public double S21MagOrRe;
-		public double S21AngOrIm;
-		public double S22MagOrRe;
-		public double S22AngOrIm;
-	}
 }
---
+++ normalizerS2Pfiles/normalizerS2Pfiles/Sample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace normalizerS2Pfiles
+{
+	struct Sample
+	{
+		public double Freq;
+		public double S11MagOrRe;
+		public double S11AngOrIm;
+		public double S12MagOrRe;
+		public double S12AngOrIm;
+		public double S21MagOrRe;
+		public double S21AngOrIm;
+		public double S22MagOrRe;
+		public double S22AngOrIm;
+	}
+}
