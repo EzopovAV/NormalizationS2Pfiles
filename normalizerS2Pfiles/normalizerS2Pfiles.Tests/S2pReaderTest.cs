4c482cb41c5ba3133066ca54b0a0663256716798
EzopovAV <EzopovAV@mail.ru>
2020-10-07 17:36:55 +0400

    Add unit test project

---
+++ normalizerS2Pfiles/normalizerS2Pfiles.Tests/S2pReaderTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace normalizerS2Pfiles.Tests
+{
+    [TestFixture]
+    public class S2pReaderTest
+    {
+        [Test]
+        public void testMethod1()
+        {
+            Assert.AreEqual(true, false);
+        }
+
+        [Test]
+        public void testMethod2()
+        {
+
+        }
+    }
+}
