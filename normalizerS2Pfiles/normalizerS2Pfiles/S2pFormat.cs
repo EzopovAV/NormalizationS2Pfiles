ce6639551563e9915886391a5f23ae125fb28d60
EzopovAV <EzopovAV@mail.ru>
2020-10-09 11:18:53 +0400

    Override Equals and GetHashCode in S2pFormat

--- normalizerS2Pfiles/normalizerS2Pfiles/S2pFormat.cs
+++ normalizerS2Pfiles/normalizerS2Pfiles/S2pFormat.cs
@@ -6,5 +6,31 @@ namespace normalizerS2Pfiles
 	{
 		public FrequencyUnits FrequencyUnits;
 		public DataUnits DataUnits;
+
+		public bool Equals(S2pFormat other)
+		{
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			if (ReferenceEquals(other, null))
+			{
+				return false;
+			}
+
+			return FrequencyUnits == other.FrequencyUnits && DataUnits == other.DataUnits;
+		}
+
+		public override bool Equals(object obj)
+		{
+			S2pFormat s2PFormat = obj as S2pFormat;
+			return Equals(s2PFormat);
+		}
+
+		public override int GetHashCode()
+		{
+			return FrequencyUnits.GetHashCode() ^ DataUnits.GetHashCode();
+		}
 	}
 }
